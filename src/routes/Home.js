9c02b7270afb06b59a74b2f42b72cd69280cbcaf
Anya L. Keller <kelleranyal@gmail.com>
2021-12-16 13:07:42 -0500

    oh no

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -15,7 +15,7 @@ function DayCols(props) {
     <div className="row">
       {[""].concat(props.dayNames).map((day, index) => (
         <div className="col" key={index}>
-          <h3>{day}</h3>
+          <h5>{day}</h5>
         </div>
       ))}
     </div>
@@ -23,7 +23,102 @@ function DayCols(props) {
 }
 
 function ScheduleGrid(props) {
-  var dayNames = ["Sun", "Mon", "Tues", "Wed", "Thr", "Fri", "Sat"];
+  let dayNames = ["Sun", "Mon", "Tues", "Wed", "Thr", "Fri", "Sat"];
+  let fiveMinBlocks = 180;
+  let courses = {
+    "Emerg Mod Europ": {
+      code: "CAS HI102",
+      crHrs: 4,
+      instructor: "Backman",
+      sections: [
+        {
+          type: "Independent",
+          section: "A1",
+          logistics: {
+            building: "CAS",
+            room: "B18",
+            start: "9:05am",
+            stop: "9:55am",
+            days: ["Mon", "Wed", "Fri"]
+          }
+        }
+      ]
+    },
+    "Italian Film": {
+      code: "CAS LI283",
+      crHrs: 4,
+      instructor: "Carter",
+      sections: [
+        {
+          type: "Independent",
+          section: "A1",
+          notes: "Mts w/CAS CI266",
+          logistics: {
+            building: "CAS",
+            room: "223",
+            start: "2:30pm",
+            stop: "3:20pm",
+            days: ["Mon", "Wed", "Fri"]
+          }
+        }
+      ]
+    },
+    "ENG Mechanics 1": {
+      code: "ENG EK301",
+      crHrs: 4,
+      instructor: "Holt",
+      sections: [
+        {
+          type: "Lecture",
+          section: "A3",
+          notes: "Class Closed",
+          logistics: {
+            building: "EPC",
+            room: "209",
+            start: "1:30pm",
+            stop: "3:15pm",
+            days: ["Tues", "Thr"]
+          }
+        },
+        {
+          type: "Discussion",
+          section: "B1",
+          instructor: "Farny",
+          notes: "Class Closed",
+          logistics: { days: ["Arr"] }
+        }
+      ]
+    },
+    "Prob Stat Data": {
+      code: "ENG EK381",
+      crHrs: 4,
+      instructor: "Nagem",
+      sections: [
+        {
+          type: "Lecture",
+          section: "A3",
+          logistics: {
+            building: "PHO",
+            room: "203",
+            start: "10:10am",
+            stop: "11:55am	",
+            days: ["Mon", "Wed"]
+          }
+        },
+        {
+          type: "Discussion",
+          section: "B8",
+          logistics: {
+            building: "CAS",
+            room: "229",
+            start: "1:25pm",
+            stop: "2:15pm",
+            days: ["Mon"]
+          }
+        }
+      ]
+    }
+  };
   return (
     <div className="container">
       <DayCols dayNames={dayNames} />
