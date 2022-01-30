5567120aaf71e56922ec5d878f5d199c30dc1004
Anya L. Keller <kelleranyal@gmail.com>
2022-01-30 00:06:46 -0500

    moved more crap into TimeHandling ayeee

--- src/components/DayTimeBlock.js
+++ src/components/DayTimeBlock.js
@@ -1,18 +1,10 @@
-import { convertTime } from "../utils/TimeHandling.js";
-const { Interval } = require("luxon");
+import { getTimeBlockPercentages } from "../utils/TimeHandling.js";
 
 function DayTimeBlock(props) {
-  const startTime = convertTime(props.sectionData.logistics.start);
-  const stopTime = convertTime(props.sectionData.logistics.stop);
-  const dayStart = startTime.startOf("day").plus({ hours: 7 });
-  var timeInterval = Interval.fromDateTimes(startTime, stopTime);
-
-  // how many minutes from 7 am
-  var sevenToStartTime = startTime.diff(dayStart, "minutes").as("minutes");
-  var minutesLong = stopTime.diff(startTime, "minutes").as("minutes");
-
-  const blockTopPercent = (100 * sevenToStartTime) / 5 / 180;
-  const blockHeightPercent = (100 * minutesLong) / 5 / 180;
+  const [blockTopPercent, blockHeightPercent] = getTimeBlockPercentages(
+    props.sectionData.logistics.start,
+    props.sectionData.logistics.stop
+  );
 
   return (
     <div
--- src/utils/TimeHandling.js
+++ src/utils/TimeHandling.js
@@ -1,9 +1,30 @@
 const { DateTime } = require("luxon");
+const { Interval } = require("luxon");
 
+// Converts h:mma e.g. 8:30am 
+// To luxon DateTime
 // timeString ""
 function convertTime(timeString) {
   var d = DateTime.fromFormat(timeString, "h:mma");
   return d;
 }
 
-export { convertTime };
+// Converts start time and a stop time
+// Outputs [css top position percentage, css height percentage ]
+// [sevenToStartTime, blockHeightPercent]
+// For grid starting at 7am and ends at 11pm
+// 5 minute chunks of time
+function getTimeBlockPercentages (start, stop){
+  const startTime = convertTime(start);
+  const stopTime = convertTime(stop);
+  const dayStart = startTime.startOf("day").plus({ hours: 7 });
+  var timeInterval = Interval.fromDateTimes(startTime, stopTime);
+
+  // how many minutes from 7 am
+  var sevenToStartTime = startTime.diff(dayStart, "minutes").as("minutes");
+  var minutesLong = stopTime.diff(startTime, "minutes").as("minutes");
+
+  return [(100 * sevenToStartTime) / 5 / 180, (100 * minutesLong) / 5 / 180];
+}
+
+export { convertTime, getTimeBlockPercentages };
